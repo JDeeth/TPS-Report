604b6dd9f5e757b90ab74b9e662d65f8e99772a5
Jack Deeth <git@jackdeeth.org.uk>
2018-08-29 01:41:57 +0100

    Fix #1 - avoid overrunning std::vector.max_size
    The issue was caused by developing on a 64-bit system and then compiling
    a 32-bit version with a lower limit on std::vector size.

--- src/tpslist.cpp
+++ src/tpslist.cpp
@@ -27,17 +27,25 @@ void TPSList::load(QMainWindow* window, QStatusBar* sb) {
     std::stringstream s;
     s.imbue(std::locale(""));
 
-    int lines = 0;
+    size_t lines = 0;
+    auto max_lines = list.max_size();
     std::string line;
 
     while (std::getline(f, line)) {
+      if (lines >= max_lines) {
+        s << "Cannot load more than " + std::to_string(max_lines) +
+                 " records on this system.";
+        QMessageBox::critical(window, "Error", QString::fromStdString(s.str()));
+        break;
+      }
+      list.emplace_back(std::regex_replace(line, std::regex(" "), ""));
+
       ++lines;
       if (lines % 100000 == 0) {
         s << lines << " TPS phone numbers imported";
         sb->showMessage(QString::fromStdString(s.str()));
         s.str("");
       }
-      list.emplace_back(std::regex_replace(line, std::regex(" "), ""));
     }
 
     f.close();
